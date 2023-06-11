d32c8ce3a55fd308fd6500eb964f2d26ac6826b9
DianaMorales03 <ruanodiana03@gmail.com>
2023-06-11 11:15:47 -0600

    consumo de api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,49 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 import Boton from "@/components/boton";
 import Descripcion from "@/components/descripcion";
 
 export default function Home() {
+  const [pais, setPais] = useState("");
+  const [monto, setMonto] = useState("");
+  const [result, setResult] = useState("");
+  useEffect(()=>{
+    console.log(pais)
+  }, [pais])
+  useEffect(()=>{
+    console.log(monto)
+  }, [monto])
+
+  const subir = async () => {
+    event.preventDefault();
+    setResult("");
+    if(monto!=="" && pais!==""){
+      try {
+        const response = await fetch("/api/generate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ comida: pais, monto: monto }),
+        });
+  
+        const data = await response.json();
+        if (response.status !== 200) {
+          throw data.error || new Error(`Request failed with status ${response.status}`);
+        }
+        console.log(data)
+        setResult(data.result);
+        setPais("");
+        setMonto("")
+      } catch (error) {
+        // Consider implementing your own error handling logic here
+        console.error(error);
+        alert(error.message);
+      }
+    }
+  }
+
   return (
     <div>
       <Head>
@@ -28,30 +67,31 @@ export default function Home() {
         <div className='presupuesto'>
           <p>Presupuesto: </p>
           <form onSubmit={ev => { ev.preventDefault; console.log(ev.target.search.value) }}>
-            <input className='formPresupuesto' type='sumbit' name='presupuesto' autoComplete='off' onChange={(event) =>console.log(event.target.value)}></input>
+            <input className='formPresupuesto' type='sumbit' name='presupuesto' autoComplete='off' onChange={(event) => setMonto(event.target.value)}></input>
           </form>
         </div>
 
         <div className='contenedor-btn-respuesta'>
           {/* Botones */}
           <div className='contenedor-botones'>
-            <Boton texto='Comida Italiana'></Boton>
-            <Boton texto='Comida Mexicana'></Boton>
-            <Boton texto='Comida Japonesa'></Boton>
-            <Boton texto='Comida China'></Boton>
-            <Boton texto='Comida Estadounidense'></Boton>
-            <Boton texto='Comida Clásicas'></Boton>
+            <Boton texto='Comida Italiana' value={pais} setValue={setPais}></Boton>
+            <Boton texto='Comida Mexicana' value={pais} setValue={setPais}></Boton>
+            <Boton texto='Comida Japonesa' value={pais} setValue={setPais}></Boton>
+            <Boton texto='Comida China' value={pais} setValue={setPais}></Boton>
+            <Boton texto='Comida Estadounidense' value={pais} setValue={setPais}></Boton>
+            <Boton texto='Comida Clásicas' value={pais} setValue={setPais}></Boton>
           </div>
           {/* Respuesta IA */}
           <div className='respuesta'>
             <p>Respuesta de la IA</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis metus eleifend, hendrerit magna vel, aliquam nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc quis velit vel tellus semper tempor. Mauris efficitur volutpat massa, in volutpat nunc. Fusce eleifend tincidunt scelerisque. Fusce eu viverra est, a iaculis tortor. Quisque quis felis vel eros pharetra bibendum ut et ipsum. Quisque dapibus tincidunt lacus, at ultricies tellus tincidunt in. Nam malesuada pulvinar purus, vitae maximus lorem fringilla a. Cras varius, leo nec condimentum gravida, lectus tellus iaculis justo, et pretium nisl lorem eget risus. </p>
+            {result}
           </div>
         </div>
         {/* Footer */}
         <div className='contenedor-footer'>
           <img src='footer.png' />
         </div>
+        <button onClick={subir}>Subir</button>
       </main>
     </div>
   );
